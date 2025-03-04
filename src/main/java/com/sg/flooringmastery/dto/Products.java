d5c4e086b47fabc3ace6e1840db30c9aa996a52f
Fang, Xin Ran <xinranfang.ap@gmail.com>
2025-03-04 14:49:20 -0500

    Commit 7: Implement Products class

--- src/main/java/com/sg/flooringmastery/dto/Products.java
+++ src/main/java/com/sg/flooringmastery/dto/Products.java
@@ -1,4 +1,41 @@
 package com.sg.flooringmastery.dto;
 
+import java.math.BigDecimal;
+
 public class Products {
+    private String ProductType;
+    private BigDecimal CostPerSquareFoot;
+    private BigDecimal LaborCostPerSquareFoot;
+
+    public Products() {}
+
+    public Products(String productType, BigDecimal costPerSquareFoot, BigDecimal laborCostPerSquareFoot) {
+        ProductType = productType;
+        CostPerSquareFoot = costPerSquareFoot;
+        LaborCostPerSquareFoot = laborCostPerSquareFoot;
+    }
+
+    public String getProductType() {
+        return ProductType;
+    }
+
+    public void setProductType(String productType) {
+        ProductType = productType;
+    }
+
+    public BigDecimal getCostPerSquareFoot() {
+        return CostPerSquareFoot;
+    }
+
+    public void setCostPerSquareFoot(BigDecimal costPerSquareFoot) {
+        CostPerSquareFoot = costPerSquareFoot;
+    }
+
+    public BigDecimal getLaborCostPerSquareFoot() {
+        return LaborCostPerSquareFoot;
+    }
+
+    public void setLaborCostPerSquareFoot(BigDecimal laborCostPerSquareFoot) {
+        LaborCostPerSquareFoot = laborCostPerSquareFoot;
+    }
 }
