5009cf38f8c04bd755bd41dac73b1c924445f83e
Fang, Xin Ran <xinranfang.ap@gmail.com>
2025-03-04 14:46:49 -0500

    Commit 6: Implement Orders class

--- src/main/java/com/sg/flooringmastery/dto/Orders.java
+++ src/main/java/com/sg/flooringmastery/dto/Orders.java
@@ -1,5 +1,131 @@
 package com.sg.flooringmastery.dto;
 
+import java.awt.geom.Area;
+import java.math.BigDecimal;
+
 public class Orders {
+    private int orderNumber;
+    private String customerName;
+    private String state;
+    private BigDecimal taxRate;
+    private String productType;
+    private BigDecimal Area;
+    private BigDecimal CostPerSquareFoot;
+    private BigDecimal LaborCostPerSquareFoot;
+    private BigDecimal MaterialCost;
+    private BigDecimal LaborCost;
+    private BigDecimal Tax;
+    private BigDecimal Total;
+
+    public Orders() {}
+
+    public Orders(int orderNumber, String customerName, String state, BigDecimal taxRate, String productType, BigDecimal area, BigDecimal costPerSquareFoot, BigDecimal laborCostPerSquareFoot, BigDecimal materialCost, BigDecimal laborCost, BigDecimal tax, BigDecimal total) {
+        this.orderNumber = orderNumber;     // read-only field
+        this.customerName = customerName;
+        this.state = state;
+        this.taxRate = taxRate;
+        this.productType = productType;
+        Area = area;
+        CostPerSquareFoot = costPerSquareFoot;
+        LaborCostPerSquareFoot = laborCostPerSquareFoot;
+        MaterialCost = materialCost;
+        LaborCost = laborCost;
+        Tax = tax;
+        Total = total;
+    }
+
+    public int getOrderNumber() {
+        return orderNumber;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public BigDecimal getTaxRate() {
+        return taxRate;
+    }
+
+    public void setTaxRate(BigDecimal taxRate) {
+        this.taxRate = taxRate;
+    }
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public void setProductType(String productType) {
+        this.productType = productType;
+    }
+
+    public BigDecimal getArea() {
+        return Area;
+    }
+
+    public void setArea(BigDecimal area) {
+        Area = area;
+    }
+
+    public BigDecimal getCostPerSquareFoot() {
+        return CostPerSquareFoot;
+    }
+
+    public void setCostPerSquareFoot(BigDecimal costPerSquareFoot) {
+        CostPerSquareFoot = costPerSquareFoot;
+    }
+
+    public BigDecimal getLaborCostPerSquareFoot() {
+        return LaborCostPerSquareFoot;
+    }
+
+    public void setLaborCostPerSquareFoot(BigDecimal laborCostPerSquareFoot) {
+        LaborCostPerSquareFoot = laborCostPerSquareFoot;
+    }
+
+    public BigDecimal getMaterialCost() {
+        return MaterialCost;
+    }
+
+    public void setMaterialCost(BigDecimal materialCost) {
+        MaterialCost = materialCost;
+    }
+
+    public BigDecimal getLaborCost() {
+        return LaborCost;
+    }
+
+    public void setLaborCost(BigDecimal laborCost) {
+        LaborCost = laborCost;
+    }
+
+    public BigDecimal getTax() {
+        return Tax;
+    }
+
+    public void setTax(BigDecimal tax) {
+        Tax = tax;
+    }
+
+    public BigDecimal getTotal() {
+        return Total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        Total = total;
+    }
+
+
 
 }
