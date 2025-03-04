78a9a7a8e1ef785a2df31756c753ea7f3a37bc23
Fang, Xin Ran <xinranfang.ap@gmail.com>
2025-03-04 14:50:55 -0500

    Commit 8: Implement Tax class

--- src/main/java/com/sg/flooringmastery/dto/Tax.java
+++ src/main/java/com/sg/flooringmastery/dto/Tax.java
@@ -1,4 +1,41 @@
 package com.sg.flooringmastery.dto;
 
+import java.math.BigDecimal;
+
 public class Tax {
+    private String StateAbbreviation;
+    private String StateName;
+    private BigDecimal TaxRate;
+
+    public Tax() {}
+
+    public Tax(String stateAbbreviation, String stateName, BigDecimal taxRate) {
+        StateAbbreviation = stateAbbreviation;
+        StateName = stateName;
+        TaxRate = taxRate;
+    }
+
+    public String getStateAbbreviation() {
+        return StateAbbreviation;
+    }
+
+    public void setStateAbbreviation(String stateAbbreviation) {
+        StateAbbreviation = stateAbbreviation;
+    }
+
+    public String getStateName() {
+        return StateName;
+    }
+
+    public void setStateName(String stateName) {
+        StateName = stateName;
+    }
+
+    public BigDecimal getTaxRate() {
+        return TaxRate;
+    }
+
+    public void setTaxRate(BigDecimal taxRate) {
+        TaxRate = taxRate;
+    }
 }
