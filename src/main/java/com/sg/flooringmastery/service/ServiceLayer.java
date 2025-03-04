e84a461cd776a2f63f6419c81c2b0a38c34c58db
Fang, Xin Ran <xinranfang.ap@gmail.com>
2025-03-04 16:01:25 -0500

    Commit 13: Add methods to ServiceLayer interface

--- src/main/java/com/sg/flooringmastery/service/ServiceLayer.java
+++ src/main/java/com/sg/flooringmastery/service/ServiceLayer.java
@@ -1,4 +1,13 @@
 package com.sg.flooringmastery.service;
 
+import java.math.BigDecimal;
+
 public interface ServiceLayer {
+    BigDecimal calculateMaterialCost(BigDecimal area, BigDecimal costPerSquareFoot);
+
+    BigDecimal calculateLaborCost(BigDecimal area, BigDecimal laborCostPerSquareFoot);
+
+    BigDecimal calculateTax(BigDecimal materialCost, BigDecimal laborCost, BigDecimal taxRate);
+
+    BigDecimal calculateTotal(BigDecimal materialCost, BigDecimal laborCost, BigDecimal tax);
 }
