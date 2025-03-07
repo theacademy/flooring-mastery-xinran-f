33b528f2beebc1d20f86599d48dbae760372f8f7
Fang, Xin Ran <xinranfang.ap@gmail.com>
2025-03-07 10:59:57 -0500

    Commit 24: Add Spring Dependency Injection

--- src/main/java/com/sg/flooringmastery/APP.java
+++ src/main/java/com/sg/flooringmastery/APP.java
@@ -7,16 +7,25 @@ import com.sg.flooringmastery.service.ServiceLayerImpl;
 import com.sg.flooringmastery.ui.UserIO;
 import com.sg.flooringmastery.ui.UserIOConsoleImpl;
 import com.sg.flooringmastery.ui.View;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class APP {
     public static void main(String[] args) {
-        UserIO io = new UserIOConsoleImpl();
-        View view = new View(io);
-        OrdersDAO ordersDAO = new OrdersDAOFilelmpl();
-        ProductsDAO productsDAO = new ProductsDAOFilelmpl();
-        TaxDAO taxDAO = new TaxDAOFilelmpl();
-        ServiceLayer service = new ServiceLayerImpl(ordersDAO, productsDAO, taxDAO);
-        Controller controller = new Controller(service, view, ordersDAO, productsDAO, taxDAO);
+//        UserIO io = new UserIOConsoleImpl();
+//        View view = new View(io);
+//        OrdersDAO ordersDAO = new OrdersDAOFilelmpl();
+//        ProductsDAO productsDAO = new ProductsDAOFilelmpl();
+//        TaxDAO taxDAO = new TaxDAOFilelmpl();
+//        ServiceLayer service = new ServiceLayerImpl(ordersDAO, productsDAO, taxDAO);
+//        Controller controller = new Controller(service, view, ordersDAO, productsDAO, taxDAO);
+//        controller.run();
+
+
+        ApplicationContext ctx =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+        Controller controller =
+                ctx.getBean("controller", Controller.class);
         controller.run();
     }
 }
